a349135529a55261f8eddf1de0aef439bc8c60c1
luca-cmd <lucasequi85@gmail.com>
2022-02-13 15:31:09 +0100

    counter_duplicates

---
+++ counter_duplicates.py
@@ -0,0 +1,13 @@
+"""count the duplicates of a word."""
+
+
+def main(string: str) -> dict[str, int]:
+    dictionary: dict[str, int] = {}
+    for c in string:
+        char_count = string.count(c)
+        if char_count > 1:
+            dictionary[c] = char_count
+    return dictionary
+
+
+print(main("casa"))
