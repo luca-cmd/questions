ac2fa42d2e06d18de2843626fa76d7cad8edf956
luca-cmd <lucasequi85@gmail.com>
2022-02-13 12:36:57 +0100

    .gitignore, remove_duplicates.py

---
+++ remove_duplicates.py
@@ -0,0 +1,7 @@
+def main(string: str):
+    list_of_chars = [char for char in string]
+    set_of_chars = set(list_of_chars)
+    return set_of_chars
+
+
+print(main('ciao io sono luca'))
--- sumOfTwo.py
+++ sumOfTwo.py
@@ -4,6 +4,7 @@ Array = list[int]
 array1 = [1, 4, 6, 7]
 array2 = [2, 5, 7, 4]
 
+
 def sum_of_two(array1: Array, array2: Array, target: int) -> bool:
     complements = set(())
     for v in array1:
@@ -14,5 +15,6 @@ def sum_of_two(array1: Array, array2: Array, target: int) -> bool:
             if v == x:
                 return True
     return False
+
 
 print(sum_of_two(array1=array1, array2=array2, target=10))
