c72819cab02a23919c71d210edcf4fa3d1eb14f5
luca-cmd <lucasequi85@gmail.com>
2022-02-16 14:24:51 +0100

    formatting

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -6,8 +6,11 @@ class Main:
     def __init__(self, matrix: Matrix) -> None:
         self.matrix = matrix
         self.rotation = 0
-        self.position = [[[2, 0], [1, 0], [0, 0]], [
-            [2, 1], [1, 1], [0, 1]], [[2, 2], [1, 2], [0, 2]]]
+        self.position = [
+            [[2, 0], [1, 0], [0, 0]],
+            [[2, 1], [1, 1], [0, 1]],
+            [[2, 2], [1, 2], [0, 2]],
+        ]
 
     def rotate_board(self):
         new_matrix: Matrix = []
@@ -16,26 +19,28 @@ class Main:
                 line: list[int] = []
                 for v in range(3):
                     line.append(
-                        self.matrix[self.position[i][v][0]][self.position[i][v][1]])
+                        self.matrix[self.position[i][v][0]][self.position[i][v][1]]
+                    )
                 new_matrix.append(line)
             self.matrix = new_matrix
             new_matrix = []
 
     def print_board(self):
         for i in self.matrix:
-            print('\n-------------\n|', end='')
+            print("\n-------------\n|", end="")
             for x in i:
-                print(f' {x} ',  end='|')
-        print('\n-------------')
+                print(f" {x} ", end="|")
+        print("\n-------------")
 
     def start(self):
         while True:
             self.print_board()
-            self.rotation = int(int(
-                input('how much you want to rotate the matrix -> ')) / 90)
+            self.rotation = int(
+                int(input("how much you want to rotate the matrix -> ")) / 90
+            )
             self.rotate_board()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main = Main(matrix=matrix)
     main.start()
